2d8e2788f7e1874407e8faeb286f36ea2398f123
Vikram-Hegde <vikramhegde1440@gmail.com>
2024-02-03 19:39:20 +0530

    chore: move layout related things to css file instead

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Logo from './assets/logo.svg'
 function App() {
 	return (
 		<>
-			<header className="bg-slate-100 px-[max(2rem,50%-1100px/2)] py-4">
+			<header className="bg-slate-100 py-5">
 				<img src={Logo} alt="logo" />
 			</header>
 		</>
