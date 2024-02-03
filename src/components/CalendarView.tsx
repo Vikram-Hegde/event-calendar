3a3c7f20584953e03a5ce6fbe8a07755a475370b
Vikram-Hegde <vikramhegde1440@gmail.com>
2024-02-03 22:32:27 +0530

    fix: index for the calendar hours ✅

--- src/components/CalendarView.tsx
+++ src/components/CalendarView.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import categorizeEvents from '../utils/categorizeEvents'
 import generateHours from '../utils/generateHours'
 
@@ -12,17 +13,17 @@ export default function CalendarView({ events }) {
 		<section className="calendar">
 			<div className="calendar__hours | text-gray-600">
 				{hours.map(({ hour, timeOfTheDay }, index) => (
-					<>
-						<div key={index} className="calendar__hour">
+					<Fragment key={index}>
+						<div className="calendar__hour">
 							<div className="calendar__hour-label | font-semibold text-lg">
 								{hour}:00{' '}
 								<span className="text-gray-400 text-sm">{timeOfTheDay}</span>
 							</div>
 						</div>
-						<div key={index + 30} className="calendar__hour">
+						<div className="calendar__hour">
 							<div className="calendar__hour-label | text-sm">{hour}:30</div>
 						</div>
-					</>
+					</Fragment>
 				))}
 			</div>
 		</section>
--- src/utils/categorizeEvents.ts
+++ src/utils/categorizeEvents.ts
@@ -15,11 +15,12 @@ type CategorizedEvents = {
  * Example: If an event starts at 9:45 AM and ends at 10:45 AM, it will be added to 9AM slot
  * 					since 10:15 is in between 9:45 and 10:45, it will be added to 9AM slot
  * @param events - Array of events
+ * @param beginWith - The time to start with, default is 9
  * @returns Object with keys as time of the day and values as events that occur at that time
  */
-export default function categorizeEvents(events: Event[]) {
+export default function categorizeEvents(events: Event[], beginWith?: number) {
 	return events.reduce<CategorizedEvents>((acc, event) => {
-		const startHour = 9 + Math.floor(event.start / 60)
+		const startHour = (beginWith ?? 9) + Math.floor(event.start / 60)
 		const startIn12HourFormat = startHour % 12 ? startHour % 12 : 12
 		const timeOfTheDay = startHour < 12 ? 'AM' : 'PM'
 		const prevHourKey = Object.keys(acc)[Object.keys(acc).length - 1]
