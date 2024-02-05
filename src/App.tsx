6ee3a12a46743fa1f7834c902a85b3ecb1fc6bb1
Vikram-Hegde <vikramhegde1440@gmail.com>
2024-02-05 08:53:06 +0530

    feat: removed ariakit and added a unstyled event form

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,45 @@ import Logo from './assets/icons/logo.svg'
 import CalendarView from './components/CalendarView'
 import Button from './components/Button'
 import AddIcon from './assets/icons/AddIcon'
-import {
-	Dialog,
-	DialogHeading,
-	Form,
-	FormError,
-	FormInput,
-	FormLabel,
-	useFormStore,
-} from '@ariakit/react'
-
-// let counter = 1
 
 function App() {
-	const [eventsList] = useState(events as EventProp[])
+	const [eventsList, setEventsList] = useState(events as EventProp[])
 	const [open, setOpen] = useState(false)
-	const eventForm = useFormStore({
-		defaultValues: { title: '', start: '', end: '' },
-	})
+
+	const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		// get the form data
+		const formData = new FormData(e.currentTarget)
+
+		function convertTimeToOffset(time: string, offsetStart: number) {
+			const [hour, minute] = time.split(':').map((time) => parseInt(time))
+			const isPM = time.includes('PM')
+			const offset = (hour - offsetStart) * 60 + minute
+			if (isPM) {
+				return offset + 12 * 60
+			}
+			return offset
+		}
+
+		// convert start-time and end-time to a number which is the offset in terms of minutes from 9:00 AM
+		const startTime = formData.get('start-time') as string
+		const endTime = formData.get('end-time') as string
+		const startOffset = convertTimeToOffset(startTime, 9)
+		const endOffset = convertTimeToOffset(endTime, 9)
+		const title = formData.get('title') as string
+
+		console.log(startOffset, endOffset)
+
+		// create the event object
+		const newEvent = {
+			title,
+			start: startOffset,
+			end: endOffset,
+		}
+		setEventsList([...eventsList, newEvent])
+		setOpen(false)
+		e.currentTarget.reset()
+	}
 
 	return (
 		<>
@@ -39,46 +60,54 @@ function App() {
 					</Button>
 				</div>
 			</header>
-			<Dialog open={open} onClose={() => setOpen(false)} className="dialog">
-				<DialogHeading>
-					<h2 className="text-2xl font-bold">Add New Event</h2>
-				</DialogHeading>
-				<Form store={eventForm} aria-labelledby="add-new-event">
-					<div className="field">
-						<FormLabel name={eventForm.names.title}>Title</FormLabel>
-						<FormInput
-							name={eventForm.names.title}
-							placeholder="Event Title"
-							required
-						/>
-					</div>
-					<div className="field">
-						<FormLabel name={eventForm.names.start}>Start time</FormLabel>
-						<FormInput
-							name={eventForm.names.start}
-							placeholder="Event Title"
-							min={0}
-							max={719}
-							required
-						/>
-						<FormError name={eventForm.names.start} className="error" />
-					</div>
-					<div className="field">
-						<FormLabel name={eventForm.names.end}>End time</FormLabel>
-						<FormInput
-							name={eventForm.names.end}
-							placeholder="Event Title"
-							min={0}
-							max={720}
-							required
-						/>
-						<FormError name={eventForm.names.end} className="error" />
-					</div>
-				</Form>
-			</Dialog>
 			<main>
 				<CalendarView events={eventsList} />
 			</main>
+			<div className="backdrop" data-backdrop={open}></div>
+			{open && (
+				<div className="modal">
+					<div className="modal__content">
+						<h2 className="text-2xl font-bold mb-5">New Event</h2>
+						<form className="flex flex-col gap-3" onSubmit={handleFormSubmit}>
+							<div className="flex flex-col gap-2">
+								<label htmlFor="title">Title</label>
+								<input type="text" name="title" id="title" className="input" />
+							</div>
+							<div className="flex flex-col gap-2">
+								<label htmlFor="start-time">Start Time</label>
+								<input
+									type="time"
+									name="start-time"
+									id="start-time"
+									step={60}
+									pattern="^(0?[1-9]|1[0-2]):[0-5][0-9] [APap][Mm]$"
+								/>
+							</div>
+							<div className="flex flex-col gap-2">
+								<label htmlFor="end-time">End Time</label>
+								<input
+									type="time"
+									name="end-time"
+									id="end-time"
+									step={60}
+									pattern="^(0?[1-9]|1[0-2]):[0-5][0-9] [APap][Mm]$"
+								/>
+							</div>
+							<div className="flex justify-end gap-3">
+								<Button
+									onClick={() => setOpen(false)}
+									className="bg-slate-600 text-slate-100"
+								>
+									Cancel
+								</Button>
+								<Button className="bg-slate-600 text-slate-100" type="submit">
+									Add
+								</Button>
+							</div>
+						</form>
+					</div>
+				</div>
+			)}
 		</>
 	)
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,18 +2,21 @@ import { ReactNode } from 'react'
 
 interface ButtonProp {
 	onClick?: () => void
-	className: string
-	children: ReactNode
+	className?: string
+	children?: ReactNode
+	type?: 'button' | 'submit' | 'reset'
 }
 export default function Button({
 	onClick = () => {},
 	className,
 	children,
+	type = 'button',
 }: ButtonProp) {
 	return (
 		<button
 			className={`py-1.5 px-2 flex items-center gap-2 rounded-md ${className}`}
 			onClick={onClick}
+			type={type}
 		>
 			{children}
 		</button>
