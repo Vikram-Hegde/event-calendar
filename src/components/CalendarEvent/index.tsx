8df262f46a2bfe8de8fd14dd2a6110e781d74805
Vikram-Hegde <vikramhegde1440@gmail.com>
2024-02-08 22:46:11 +0530

    chore: match design colors

--- src/components/CalendarEvent/index.tsx
+++ src/components/CalendarEvent/index.tsx
@@ -26,11 +26,11 @@ export default function CalendarEvent({
 	return (
 		<div
 			key={item.title + item.start + Math.random() * 100}
-			className="calendar__event | absolute overflow-hidden grid gap-1.5 content-start bg-white p-1.5 border border-solid border-gray-200 border-l-2 border-l-blue-900 rounded-e"
+			className="calendar__event | absolute overflow-hidden grid gap-1.5 content-start bg-white p-1.5 border border-solid border-gray-200 border-l-4 border-l-[#181e30] rounded-e"
 			style={eventDimensions}
 		>
 			<div
-				className="calendar__event-title | leading-[1cap] text-sm text-blue-500 font-semibold"
+				className="calendar__event-title | leading-[1cap] text-sm text-[#181e30] font-semibold"
 				title={`${item.title} - ${item.start / 60 + ':' + (item.start % 60)}:${
 					item.end
 				}`}
